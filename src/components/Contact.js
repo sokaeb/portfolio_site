e6d1544f93e05d566efa0dd48aa4d28bd8bffa58
sokaeb <sonja.k.benton@gmail.com>
2021-04-26 09:54:38 -0400

    updated resume target link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -44,7 +44,7 @@ function Contact() {
                     </div>
                     </>
           ) : (
-            <a href="https://docs.google.com/document/d/12AB_ZTF-1NR98qcF7Ak_qb6mJsWtQPcSGn2BB479UcU/edit?usp=sharing" target="_blank" rel="noreferrer"><button type="button" class="btn btn-lg text-white resumeBtn" data-bs-target="#resume">
+            <a href="https://docs.google.com/document/d/1yK4UfMK_mfvT1kJh4YxrDBm7B3NQXlvELI-3Qiut_Js/edit?usp=sharing" target="_blank" rel="noreferrer"><button type="button" class="btn btn-lg text-white resumeBtn" data-bs-target="#resume">
             View Resume
           </button></a>
           )}
